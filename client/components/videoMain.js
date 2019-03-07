b826301e4eaa309ac859532a8d59e7085982e99f
kylebirns <kylebirns@gmail.com>
2019-03-07 15:05:19 -0500

    material ui loading in

--- client/components/videoMain.js
+++ client/components/videoMain.js
@@ -1,9 +1,9 @@
 import VideoComponent from './videoComponent'
 import React, {Component} from 'react'
-// import AppBar from 'material-ui/AppBar';
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-// import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
 
@@ -14,12 +14,12 @@ export default class VideoMain extends Component {
 
   render() {
     return (
-      // <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-      <div>
-        {/* <AppBar title="React Twilio Video" /> */}
-        <VideoComponent />
-      </div>
-      // </MuiThemeProvider>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <div>
+          <AppBar title="React Twilio Video" />
+          <VideoComponent />
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
